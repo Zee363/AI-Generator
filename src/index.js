51b45b9dfde0501a2d1aeb6b37b24551c803aeb2
Zinhle Nhlapo <zinhle.nhlapo31@gmail.com>
2025-05-17 05:29:07 +0200

    Added the basic JavaScript.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+function generateVerse(event) {
+  event.preventDefault();
+  console.log("Form submitted");
+
+new Typewriter('#verse', {
+  strings: 'John 3:16 - For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.',
+  autoStart: true,
+  delay: 0.5,
+  cursor: ""
+});
+};
+
+
+let bibleVerseFormElement = document.querySelector("#bible-verse-form");
+bibleVerseFormElement.addEventListener("submit", generateVerse);
