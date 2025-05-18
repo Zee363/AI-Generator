fa36129db9be9977d8c3349de8adf943ae1e3267
Zinhle Nhlapo <zinhle.nhlapo31@gmail.com>
2025-05-18 20:13:38 +0200

    Improved the user experience.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayVerse(response) {
 new Typewriter('#verse', {
   strings: response.data.answer,
   autoStart: true,
-  delay: 5,
+  delay: 0.2,
   cursor: ""
 
 });
@@ -14,10 +14,14 @@ function generateVerse(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "4b230af38fdc2b34b014a5b47d0e5tob";
-  let context ="You are a bible verse generator. You will generate a verse from the book the user inserts. The user will insert the book name and you will generate a verse from that book. Make sure to usethe user instructions.";
+  let context ="You are a bible verse generator. You will generate a verse from the book the user inserts. The user will insert the book name and you will generate a verse from that book. Make sure to usethe user instructions. Please also ensure that the sign is on a  blank new line always.";
   let prompt =`User instructions: Give me a  verse from the book the user inserts ${instructionsInput.value} and give the book and verse please. Be as brief as possible. PLease sign SheCodes AI at the end of the verse on a new line in a strong tag and purple in color`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
+
+  let verseElement = document.querySelector("#verse");
+  verseElement.classList.remove("hidden");
+  verseElement.innerHTML =  `<i style="font-size:18px" class="fa">&#xf110; <span class = "generating">Generating verse...</span></i>`;
 
   console.log("Generating verse" );
   console.log(`Prompt: ${prompt}`);
